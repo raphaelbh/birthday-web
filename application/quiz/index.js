053b24e74fb258e6485404e34eccb628d0def0f5
Raphael Oliveira <raphaeldias.ti@gmail.com>
2023-08-02 11:03:14 -0300

    feat: improvements

--- application/quiz/index.js
+++ application/quiz/index.js
@@ -8,3 +8,29 @@ const quizSent = localStorage.getItem('quiz_sent');
 if (quizSent) {
     quizButton.style.display = 'none';
 }
+
+const table = document.getElementById("rank-table");
+const tbody = table.querySelector("tbody");
+
+function addItem(position, name, score) {
+    const row = `<tr><th scope="row">${position}</th><td>${name}</td><td>${score}</td></tr>`;
+    tbody.innerHTML += row;
+}
+
+
+fetch("https://birthday-api-y1wf.onrender.com/rank")
+  .then(response => {
+    if (!response.ok) {
+      throw new Error(`Erro na requisição: ${response.status}`);
+    }
+    return response.json(); 
+  })
+  .then(data => {
+    //console.log(data);
+    for (let i = 0; i < data.size(); i++) {
+        addItem(data[i].position, data[i].user, data[i].score)
+    }
+  })
+  .catch(error => {
+    console.error("Erro:", error);
+  });
