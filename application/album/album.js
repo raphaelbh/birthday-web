8e228edbff688d5f4bacbab1ab77cba189baa1de
Raphael Oliveira <raphaeldias.ti@gmail.com>
2023-08-04 13:09:41 -0300

    feat: validation improvements

--- application/album/album.js
+++ application/album/album.js
@@ -2,15 +2,31 @@
 localStorage.removeItem("theme");
 
 
-
 const imagesInput = document.getElementById('images-input');
 const uploadButton = document.getElementById('upload-button');
 
-
 uploadButton.addEventListener('click', async () => {
-    
+
+    if (imagesInput.files.length === 0) {
+        _showError("Informe ao menos uma imagem");
+        return;
+    }
+
+    if (imagesInput.files.length > 10) {
+        _showError("Informe no máximo 10 imagens");
+        return;
+    }
+
+
     const formData = new FormData();
     for (const file of imagesInput.files) {
+
+        const isImage = await _isFileAnImage(file);
+        if (!isImage) {
+            _showError(`${file.name} não é uma imagem`);
+            return;
+        }
+
         formData.append('images[]', file);
     }
 
@@ -22,20 +38,27 @@ uploadButton.addEventListener('click', async () => {
         });
 
         if (response.ok) {
-            //statusMessage.textContent = 'Imagens enviadas com sucesso!';
-            //statusMessage.style.color = 'green';
             window.location.href = 'index.html';
         } else {
-            //statusMessage.textContent = 'Erro ao enviar imagens.';
-            //statusMessage.style.color = 'red';
+            console.error('Erro ao fazer upload:', error);
+            _showError(`Erro ao enviar imagem`);
+            return;
         }
     } catch (error) {
         console.error('Erro ao fazer upload:', error);
-        //statusMessage.textContent = 'Erro ao enviar imagens.';
-        //statusMessage.style.color = 'red';
+        _showError(`Erro ao enviar imagem`);
+        return;
     }
 });
 
+
+function _showError(message) {
+    const errorMessage = document.getElementById('error-message');
+    errorMessage.innerHTML = message;
+
+    const errorSection = document.getElementById('error-section');
+    errorSection.style.display = "block";
+}
 
 
 const photosSection = document.getElementById("photos-section");
@@ -53,8 +76,6 @@ function _addItem(image) {
 
 }
 
-
-
 const url = "https://birthday-api-y1wf.onrender.com/photos";
 fetch(url).then(response => {
     if (!response.ok) {
@@ -69,3 +90,25 @@ fetch(url).then(response => {
     console.error("Erro:", error);
 });
 
+
+async function _isFileAnImage(file) {
+    return new Promise((resolve) => {
+        const reader = new FileReader();
+        
+        reader.onload = (event) => {
+            const img = new Image();
+            
+            img.onload = () => {
+                resolve(true);
+            };
+            
+            img.onerror = () => {
+                resolve(false);
+            };
+            
+            img.src = event.target.result;
+        };
+        
+        reader.readAsDataURL(file);
+    });
+}
